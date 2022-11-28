56e3767488a0755ba598ffed191077468aeae51f
Abiodun Adefila <adefaze@gmail.com>
2022-11-28 17:42:20 +0100

    database fixes

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Task Manager App')
+
+
+const express = require('express')
+const app = express()
+
+const tasks = require('./routers/tasks')
+
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+
+//middleware
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Task Manager')
+})
+
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 5000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening to Port ${port}...`))
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+
+start()
